ae4ac67552d5774f652417d11ff7499332712450
Codezz <112660193+Codezz-ops@users.noreply.github.com>
2023-08-02 09:36:04 -0400

    Update echo.sh

--- scripting/echo.sh
+++ scripting/echo.sh
@@ -1,3 +1,15 @@
 #!/bin/bash
 
-echo "$1"
+# Function definition for echoln
+function echoln {
+  echo "$1"
+}
+
+# Check if a file name is provided as an argument
+if [ $# -eq 0 ]; then
+  echo "Usage: $0 <script.ps>"
+  exit 1
+fi
+
+# Execute the script using the 'source' command
+source "$1"
