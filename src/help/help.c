857cb66a4b825fd994382ecdfffb4a31eb8bc77a
Codezz <112660193+Codezz-ops@users.noreply.github.com>
2023-08-02 13:24:11 -0400

    Update help.c

--- src/help/help.c
+++ src/help/help.c
@@ -20,3 +20,12 @@ void whoisHelp() {
 void dirbHelp() {
     puts("Usage: dirb <URL>");
 }
+
+void Help() {
+    puts("Port Scanner (scanner): Scan for open ports on a target system.");
+    puts("ICMP Ping (ping): Send ICMP echo requests to check if a host is up.");
+    puts("DNS Enumeration (dnslookup): Perform DNS enumeration on a domain to gather information.")
+    puts("WHOIS Lookup (whois): Retrieve WHOIS information for a domain.");
+    puts("Packet Sniffer (sniffer): Capture and analyze network packets.");
+    puts("Subnet Calculator (subnet): Calculate subnet details and IP ranges.");
+}
