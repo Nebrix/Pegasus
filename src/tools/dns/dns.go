cdae6fb86e7720bed717e13243d60f7e71f3ed0d
Codezz <112660193+Codezz-ops@users.noreply.github.com>
2023-08-01 20:07:40 -0400

    Add files via upload

---
+++ src/tools/dns/dns.go
@@ -0,0 +1,71 @@
+package main
+
+import (
+	"fmt"
+	"net"
+	"os"
+	"strings"
+)
+
+func main() {
+	if len(os.Args) != 2 {
+		fmt.Println("Usage: go run main.go <domain>")
+		os.Exit(1)
+	}
+
+	domain := os.Args[1]
+
+	// Perform DNS lookup
+	records, err := net.LookupIP(domain)
+	if err != nil {
+		fmt.Printf("Error: %v\n", err)
+		os.Exit(1)
+	}
+
+	// Print IP addresses
+	fmt.Printf("IP addresses for %s:\n", domain)
+	for _, ip := range records {
+		fmt.Println(ip)
+	}
+
+	// Perform DNS lookup for other record types
+	fmt.Printf("\nOther DNS records for %s:\n", domain)
+
+	// NS records
+	nsRecords, err := net.LookupNS(domain)
+	if err == nil {
+		for _, ns := range nsRecords {
+			fmt.Printf("NS: %s\n", ns.Host)
+		}
+	}
+
+	// MX records
+	mxRecords, err := net.LookupMX(domain)
+	if err == nil {
+		for _, mx := range mxRecords {
+			fmt.Printf("MX: %s, Preference: %d\n", mx.Host, mx.Pref)
+		}
+	}
+
+	// SOA record (extracted from TXT records)
+	txtRecords, err := net.LookupTXT(domain)
+	if err == nil {
+		for _, txt := range txtRecords {
+			if strings.HasPrefix(txt, "v=spf1") {
+				soaParts := strings.Fields(txt)
+				if len(soaParts) >= 7 {
+					fmt.Printf("SOA: Primary NS: %s, Responsible person's mailbox: %s\n", soaParts[6], soaParts[1])
+				}
+				break
+			}
+		}
+	}
+
+	// TXT records
+	txtRecords, err = net.LookupTXT(domain)
+	if err == nil {
+		for _, txt := range txtRecords {
+			fmt.Printf("TXT: %s\n", txt)
+		}
+	}
+}
---
+++ src/tools/drib/dirb.go
@@ -0,0 +1,62 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"net/http"
+	"os"
+)
+
+func checkDirExists(url string) bool {
+	response, err := http.Get(url)
+	if err != nil {
+		return false
+	}
+	defer response.Body.Close()
+
+	// Check if the HTTP status code is 200 (OK) or 301 (Moved Permanently)
+	if response.StatusCode == http.StatusOK || response.StatusCode == http.StatusMovedPermanently {
+		return true
+	}
+	return false
+}
+
+func main() {
+	if len(os.Args) < 2 {
+		fmt.Println("Usage: go run main.go <URL>")
+		return
+	}
+
+	baseURL := os.Args[1]
+
+	// Read the wordlist from the file
+	file, err := os.Open("wordlist.txt")
+	if err != nil {
+		fmt.Println("Error opening wordlist.txt:", err)
+		return
+	}
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+	var directories []string
+
+	// Store each line (word) from the file in the directories slice
+	for scanner.Scan() {
+		directories = append(directories, scanner.Text())
+	}
+
+	if err := scanner.Err(); err != nil {
+		fmt.Println("Error reading wordlist.txt:", err)
+		return
+	}
+
+	fmt.Println("Directory buster started...")
+
+	// Iterate over the directories and make requests
+	for _, dir := range directories {
+		url := baseURL + "/" + dir
+		if checkDirExists(url) {
+			fmt.Printf("[%s] Directory found: %s\n", http.MethodGet, url)
+		}
+	}
+}
