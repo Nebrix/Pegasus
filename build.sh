f69a79efa6766192349d0b36d113a5006c750524
Codezz-ops <aidenpearce000000@gmail.com>
2023-08-03 10:21:29 -0400

    Added remove for new spec file

--- build.sh
+++ build.sh
@@ -9,7 +9,7 @@ requirements_file="requirements.txt"
 
 clean() {
     echo "removing..."
-    rm -rf pegasus scanner ping dns subnet dist/ build/ subnet.spec whois dirb ip.spec sniffer.spec hash.spec genhash.spec
+    rm -rf pegasus scanner ping dns subnet dist/ build/ subnet.spec whois dirb ip.spec lsniffer.spec hash.spec genhash.spec
 }
 
 install_pip() {
